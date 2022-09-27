8fb8543d0480da30754c4359e63562a68353f068
ShinigamiRyuk419 <112525531+anapivallega@users.noreply.github.com>
2022-09-27 13:46:05 -0700

    okeeeee

---
+++ Main.java
@@ -0,0 +1,52 @@
+/*1st Activity
+
+1. implement an ArrayList and HashSet
+2. Ask for user input 5 times, and then add the values into the ArrayList and HashSet
+3. Display the values in the arraylist and HashSet.
+4. Create repository for github and push the code. */
+
+import java.util.Scanner;
+import java.util.List;
+import java.util.ArrayList;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        
+        List<String> details = new ArrayList<String>();
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("NAME: ");
+        String d = input.nextLine();
+        details.add(d);
+
+        System.out.println("NICKNAME: ");
+        String d1 = input.nextLine();
+        details.add(d1);
+
+        System.out.println("RESIDENCY: ");
+        String d2 = input.nextLine();
+        details.add(d2);
+
+        System.out.println("HOBBIES: ");
+        String d3 = input.nextLine();
+        details.add(d3);
+
+        System.out.println("FAVORITES: ");
+        String d4 = input.nextLine();
+        details.add(d4);
+
+        System.out.println("Hi");
+
+
+
+
+
+
+
+
+    }
+
+}
+
